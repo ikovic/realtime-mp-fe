487b597e3b64b01ae66daf92fd292cb3be61d0af
Bruno Vego <bruno@dump.hr>
2018-02-22 10:55:17 +0100

    Structure commit (#4)
    * Pre eject commit
    
    * Structure commit
    
    * Imported regenerator runtime and added ramda
    
    * Removed service worker. Updated packages

---
+++ src/routes/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Routes extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="f1">routes go here</h1>
+      </div>
+    );
+  }
+}
+
+export default Routes;
