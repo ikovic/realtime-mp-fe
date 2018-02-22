7ab12f056543365fe865de6dd7d086f567d098e1
Andrea Sikimic <andrea.sikimic@kiwi.com>
2018-02-22 14:17:33 +0100

    adding state

--- src/routes/User/components/EditUser/index.jsx
+++ src/routes/User/components/EditUser/index.jsx
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
 
+const emptyUser = {
+  fullName: '',
+  nickname: '',
+  avatar: '',
+};
+
 class EditProfile extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: props.user || emptyUser,
+    };
+  }
+
+  handleChange = event => {
+    const key = event.target.name;
+    const value = event.target.value;
+    this.setState({ user: { ...this.state.user, [key]: value } });
+  };
+
   render() {
+    const { fullName, nickname, avatar } = this.state.user;
+    const { save } = this.props;
+
     return (
       <div>
         <h1 className="f3 bg-near-white br3 br--top black-60 mv0 pv2 ph3 tc">Edit your profile</h1>
@@ -9,20 +32,34 @@ class EditProfile extends Component {
           <form className="pa4 mw6">
             <div>
               <label className="f5 db mb2">Full name</label>
-              <input className="ba b--black-20 pa2 mb2 db w-100" />
+              <input
+                className="ba b--black-20 pa2 mb2 db w-100"
+                value={fullName}
+                onChange={this.handleChange}
+                name="fullName"
+              />
             </div>
             <div>
               <label className="f5 db mb2">Nickname</label>
-              <input className="ba b--black-20 pa2 mb2 db w-100" />
+              <input
+                className="ba b--black-20 pa2 mb2 db w-100"
+                value={nickname}
+                onChange={this.handleChange}
+                name="nickname"
+              />
             </div>
             <div>
               <label className="f5 db mb2">Avatar</label>
-              <input type="file" accept="image/*" />
+              <input
+                type="file"
+                accept="image/*"
+                value={avatar}
+                onChange={this.handleChange}
+                name="avatar"
+              />
             </div>
             <div className="flex justify-end pa3">
-              <a className="f6 link dim ba ph3 pv2 mb2 dib black">
-                Save
-              </a>
+              <a className="f6 link dim ba ph3 pv2 mb2 dib black" onClick={save}>Save</a>
             </div>
           </form>
         </div>
